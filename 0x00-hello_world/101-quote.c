df4731fc74911f87c087223ff47d8b0691115e56
cristian7atehortua <3913@holbertonschool.com>
2021-09-30 14:00:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-char f[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char f[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, f, 59);
 return (1);
 }
