e11dacf79f53e7244f0faf0605f9131f3effefe0
cristian7atehortua <3913@holbertonschool.com>
2021-09-30 13:58:31 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+char f[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, f, 59);
+return (1);
+}
