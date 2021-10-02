e59d18c302b0f71ef34a8aa7d61ed3db82066b85
cristian7atehortua <3913@holbertonschool.com>
2021-10-02 12:53:39 -0700

    commit_6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,21 @@
-}#include <stdio.h>
-/**
-* main - while practice
-*
-* Return: 0
-*/
+/*
+ * File: 6-print_numberz.c
+ * Auth: Brennan D Baraban
+ */
 
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        only using putchar and without char variables
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-int s;
-s = '0';
- for(s='0'; s<= '9'; s++)
-{
-putchar(s);
-}
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
 putchar('\n');
 return (0);
 }
