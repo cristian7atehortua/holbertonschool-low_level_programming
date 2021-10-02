f4c5d7d08488418aee213645dfed40adf4cf50b2
cristian7atehortua <3913@holbertonschool.com>
2021-10-01 21:52:14 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - while practice
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+int s;
+s = '0';
+ for(s='0'; s<= '9'; s++)
+{
+putchar(s);
+}
+putchar('\n');
+return (0);
+}
