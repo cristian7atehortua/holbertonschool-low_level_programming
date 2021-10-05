5a582e7a1f8e21deb2127530ccef6d74176a6b9b
cristian7atehortua <3913@holbertonschool.com>
2021-10-05 13:29:19 -0700

    commit_9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+  * times_table - Prints 9 times tables, starting with 0
+  * Return: void
+  */
+void times_table(void)
+{
+int i, j, k, l;
+
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+k = i * j;
+if (j == 0)
+{
+_putchar(k + '0');
+}
+
+if (k < 10 && j != 0)
+{
+_putchar(',');
+for (l = 0; l <= 1; l++)
+{
+_putchar(' ');
+}
+_putchar(k + '0');
+}
+else if (k >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
