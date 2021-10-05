59be70dda62dfc9bca4dd1fcce03b023da5fc92d
cristian7atehortua <3913@holbertonschool.com>
2021-10-05 13:18:44 -0700

    commit_8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,25 +5,23 @@
  * jack_bauer - Every minute of a day.
  * Return: no return
  */
-
 void jack_bauer(void)
 {
-int h, m, hi, hd, mi, md;
-
-for (h = 0; h < 24; h++)
-{
-hi = h / 10;
-hd = h % 10;
-for (m = 0; m < 60; m++)
-{
-mi = m / 10;
-md = m % 10;
-_putchar('0' + hi);
-_putchar('0' + hd);	
-_putchar(':');
-_putchar('0' + mi);
-_putchar('0' + md);
-_putchar('\n');
-}
-}
+	int h, m, hi, hd, mi, md;
+	for (h = 0; h < 24; h++)
+	{
+		hi = h / 10;
+		hd = h % 10;
+		for (m = 0; m < 60; m++)
+		{
+			mi = m / 10;
+			md = m % 10;
+			_putchar('0' + hi);
+			_putchar('0' + hd);
+			_putchar(':');
+			_putchar('0' + mi);
+			_putchar('0' + md);
+			_putchar('\n');
+		}
+	}
 }
