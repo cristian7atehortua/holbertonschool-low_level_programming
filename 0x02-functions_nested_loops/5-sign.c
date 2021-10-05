9ab063e4a95c061833d7af1c19bfaba4cb58ccfc
cristian7atehortua <3913@holbertonschool.com>
2021-10-05 12:16:27 -0700

    commit_5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,25 @@
 #include "main.h"
 /**
- * print_sign - prints the sign of a number
- * @n: the caracter to compere 
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ *print_sign - Prints the sign of a number
+ *@n: The character to compere
+ *Return: 1 if c is alphabetic character
+ *On error: 0 is returned
  */
-
-
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+int ret = 0;
+if (n > 0)
+{
+ret = 1;
+_putchar('+');
+} else if (n == 0)
+{
+ret = 0;
+_putchar('0');
+} else
+{
+ret = -1;
+_putchar('-');
+}
+return (ret);
 }
