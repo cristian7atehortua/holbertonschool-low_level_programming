2581d0cc44c077e833c25ceff5959496e6d317d7
cristian7atehortua <3913@holbertonschool.com>
2021-10-08 13:21:11 -0700

    commit_0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- *_isupper - checks for character
+ *_isupper - checks for uppercase character
  *@c : int
  *Return: 0, 1
  */
 
+
 int _isupper(int c)
 {
+
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
