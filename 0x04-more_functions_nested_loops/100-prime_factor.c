d074c7b655140300376f98b5c9956d7836344f66
cristian7atehortua <3913@holbertonschool.com>
2021-10-08 19:37:00 -0700

    commit_100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - print 
+ *main - write a program that finds and prints the largest prime
+ *factor of the number 612852475143, followed by a new line.
  *
- * Return: Always 0.
+ *Return: 0
  */
 
 int main(void)
 {
-long int ent = 612852475143;
-long int pri;
+	long a;
+	long fact = 612852475143;
+	long lp = 1;
 
-for (pri = ent; pri <= ent; pri++)
-{
-if (ent % pri == 0)
-{
-printf("%ld", pri);
-break;
-}
-}
-printf("\n");
-return (0);
-}
+	for (a = 2; a <= fact; a++)
+		while (fact % a == 0)
+		{
+			fact = fact / a;
+			if (a > lp)
+			{
+				lp = a;
+			}
+		}
+	printf("%ld\n", lp);
+
+	return (0);
+
