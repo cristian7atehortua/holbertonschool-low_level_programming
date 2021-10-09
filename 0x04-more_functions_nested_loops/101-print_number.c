ff63960acaca462affc79b78f29177546a57078c
cristian7atehortua <3913@holbertonschool.com>
2021-10-08 19:58:27 -0700

    commit_101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,69 +1,22 @@
 #include "main.h"
-
 /**
- * print_number - prints an integer.
- * @n: input
- * Return: multiples print
+ * print_number - takes an integer n long and prints its digits
+ * one by one recusively.
  *
- *
- *
- *void print_number(int n)
- *{
- *
- *	if (n < 0)
- *	{
- *		_putchar('-');
- *		n = n * -1;
- *	}
- *	if (n < 10)
- *	{
- *		_putchar(n + '0');
- *	}
- *	if (n < 100 &&  n > 9)
- *	{
- *		_putchar(n / 10 + '0');
- *		_putchar(n % 10 + '0');
- *	}
- *	if (n < 1000 && n > 99)
- *	{
- *		_putchar(n / 100 + '0');
- *		_putchar((n / 10) % 10 + '0');
- *		_putchar(n % 10 + '0');
- *	}
- *	if (n < 10000 && n > 999)
- *	{
- *		_putchar(n / 1000 + '0');
- *		_putchar((n / 100) % 10 + '0');
- *		_putchar((n / 10) % 10 + '0');
- *		_putchar(n % 10 + '0');
- *	}
-}*/
-
+ * @n: number to be printed
+ */
 void print_number(int n)
 {
-	unsigned int m, d, c;
+	unsigned int pos = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
+		_putchar('-');
+		pos = -n;
 	}
 
-	d = m;
-	c = 1;
+	if (n / 10)
+		print_number(pos / 10);
 
-	while (d > 9)
-	{
-		d /= 10;
-		c *= 10;
-	}
-
-	for (; c >= 1; c /= 10)
-	{
-		_putchar(((m / c) % 10) + 48);
-	}
+	_putchar(pos % 10 + '0');
 }
