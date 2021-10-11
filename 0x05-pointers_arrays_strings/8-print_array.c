a91d969360d62e46b3a54497c2d802ecefa15bcf
cristian7atehortua <3913@holbertonschool.com>
2021-10-11 16:42:27 -0700

    commit_8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *print_array - write a function that prints n elements of an array of integers.
+ *@a: pointer integer
+ *@n: parameter integer
+ *Return: void
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (i < n - 1)
+{
+printf("%d, ", a[i]);
+}
+else
+printf("%d", a[i]);
+}
+printf("\n");
+}
