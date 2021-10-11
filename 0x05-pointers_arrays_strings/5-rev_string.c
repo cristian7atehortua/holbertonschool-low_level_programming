7701466c88ebd4446964829c11e6f8e17db0d404
cristian7atehortua <3913@holbertonschool.com>
2021-10-11 16:20:08 -0700

    commit_5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<string.h>
+/**
+ *  *rev_string - display revert string data
+ *  *@s: pointer char
+ *  *Return: void
+ */
+void rev_string(char *s)
+{
+int n, i, x;
+i = 0;
+n = strlen (s);
+while (i <  n / 2)
+{
+x = s[i];
+s[i] = s[n - i - 1];
+s[n - i - 1] = x;
+i++;
+}
+}
