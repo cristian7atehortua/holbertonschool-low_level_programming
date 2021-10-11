7a7b4884ed57ccc9f3e17abcfa3c19c93f5bb06a
cristian7atehortua <3913@holbertonschool.com>
2021-10-11 16:14:58 -0700

    commit_4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<string.h>
+/**
+ *print_rev - display revert string data.
+ *@s: pointer char
+ *Return: void
+*/
+
+void print_rev(char *s)
+{
+int i;
+i = strlen(s) - 1;
+while ((s[i] != '\0') && (i >= 0))
+{
+putchar(s[i]);
+i--;
+}
+putchar('\n');
+}
