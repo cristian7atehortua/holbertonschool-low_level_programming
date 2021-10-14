24ce154755cc04e289c65cf7696e4d105ab23717
cristian7atehortua <3913@holbertonschool.com>
2021-10-14 15:06:46 -0700

    commit_4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+
+#include"main.h"
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @s: string
+ * Return: string with all letters capitalized
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+
