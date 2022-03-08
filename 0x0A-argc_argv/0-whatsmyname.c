ef41087b6a6885c9345144691f8d28f3000ba246
cristian7atehortua <3913@holbertonschool.com>
2022-03-08 10:05:29 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-int main( int argc, char *argv[i]
+int main(int argc, char *argv[i])
 	  {
-	    for(int i = 0; i < argc ; i++)
-	      {
-		printf("%s \n", argv[argc - 1]);
-	      }
+	    printf("%s\n", *argv);
 
 	    return 0;
 	  }
