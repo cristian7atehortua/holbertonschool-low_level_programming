5e02c3d911b7a6f8d0fcaadbb52ec1a9aa066f5a
cristian7atehortua <3913@holbertonschool.com>
2022-03-08 10:22:12 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 
-/**
- * main - returns the name of the functionn
- * @argc: number of inputs on the command line
- * @argv: array with the elements of the command line
- *
- * Return: 0.
- */
 
-int main(__attribute__((unused)) int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   printf("%s\n", argv[0]);
   return (0);
