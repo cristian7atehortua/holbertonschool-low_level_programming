7a99b97384b088d2b1fdf4659151e02a6478d8a1
cristian7atehortua <3913@holbertonschool.com>
2022-03-08 10:39:01 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,9 @@
  *@argv: array
  *return: 0
  */
-int main(int argc, char *argv[]){
-  int i;
-  for (i = 0; i < argc; i++)
-    printf("%s\n", argv[i]);
+int main(int argc __attribute__((unused)), char *argv[])
+{
+  printf("%s\n", argv[0]);
   return (0);
 }
   
